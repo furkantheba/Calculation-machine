c86141395ef2aaf7fefc6ef98751787a3e28eddc
Furkan <furkanbalci@msn.com>
2026-02-28 01:25:56 +0300

    Second Version: Collected repeated code sequences in islemYap() function.Encapsulation

--- app/src/main/java/com/example/fonksiyonaltndatoplamapart1/MainActivity.java
+++ app/src/main/java/com/example/fonksiyonaltndatoplamapart1/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.fonksiyonaltndatoplamapart1;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,95 +15,54 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+    String islem;
+    TextView res;
+    EditText input1,input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText input1 = findViewById(R.id.numaraInput1);
-        EditText input2 = findViewById(R.id.numaraInput2);
+        input1 = findViewById(R.id.numaraInput1);
+        input2 = findViewById(R.id.numaraInput2);
         Button butonArti=findViewById(R.id.hesaplaButonArti);
         Button butonEksi=findViewById(R.id.hesaplaButonEksi);
         Button butonBolu=findViewById(R.id.hesaplaButonBolu);
         Button butonCarpi=findViewById(R.id.hesaplaButonCarpi);
         Button clear1=findViewById(R.id.clearButon);
-        TextView res = findViewById(R.id.sonucYazisi);
+        res = findViewById(R.id.sonucYazisi);
         butonBolu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String deger1=input1.getText().toString();
-                String deger2=input2.getText().toString();
-                if(!(deger1.isEmpty()||deger2.isEmpty())) {
-
-                }
-                else{
-                    return;
-                }
-                double sayi1=Double.parseDouble(deger1);
-                double sayi2=Double.parseDouble(deger2);
-                double sonuc=sayi1/sayi2;
-                res.setText("Sonuç: " + sonuc);
-
+                islem="/";
+                islemYap(islem);
             }
         });
         butonArti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String deger1=input1.getText().toString();
-                String deger2=input2.getText().toString();
-                if(!(deger1.isEmpty()||deger2.isEmpty())) {
+                islem="+";
+                islemYap(islem);
 
-                }
-                else{
-                    return;
-                }
-                double sayi1=Double.parseDouble(deger1);
-                double sayi2=Double.parseDouble(deger2);
-                double sonuc=sayi1+sayi2;
-                res.setText("Sonuç: " + sonuc);
 
             }
         });
         butonEksi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String deger1=input1.getText().toString();
-                String deger2=input2.getText().toString();
-                if(!(deger1.isEmpty()||deger2.isEmpty())) {
+                islem="-";
+                islemYap(islem);
 
-                }
-                else{
-                    return;
-                }
-                double sayi1=Double.parseDouble(deger1);
-                double sayi2=Double.parseDouble(deger2);
-                double sonuc=sayi1-sayi2;
-                if(sonuc <0){
-                    res.setTextColor(Color.parseColor("#000000"));
-                }
-                else{
-                    res.setTextColor(Color.parseColor("#ffffff"));
-                }
-                res.setText("Sonuç: " + sonuc);
 
             }
         });
         butonCarpi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String deger1=input1.getText().toString();
-                String deger2=input2.getText().toString();
-                if(!(deger1.isEmpty()||deger2.isEmpty())) {
-
-                }
-                else{
-                    return;
-                }
-                double sayi1=Double.parseDouble(deger1);
-                double sayi2=Double.parseDouble(deger2);
-                double sonuc=sayi1*sayi2;
-                res.setText("Sonuç: " + sonuc);
+                islem="x";
+                islemYap(islem);
 
             }
         });
@@ -119,6 +78,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+    void islemYap(String islem){
+        String deger1=input1.getText().toString();
+        String deger2=input2.getText().toString();
+        if(!(deger1.isEmpty()||deger2.isEmpty())) {
+
+        }
+        else{
+            return;
+        }
+        double sayi1=Double.parseDouble(deger1);
+        double sayi2=Double.parseDouble(deger2);
+        double sonuc=0;
+        if(islem.equals("+")){
+            sonuc=sayi1+sayi2;
+        }
+        if(islem.equals("-")){
+            sonuc=sayi1-sayi2;
+        }
+        if(islem.equals("x")){
+            sonuc=sayi1*sayi2;
+        }
+        if(islem.equals("/")){
+            if(sayi2==0){
+                Toast.makeText(MainActivity.this,"2. sayı 0 olamaz.",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            sonuc=sayi1/sayi2;
+        }
+        if(sonuc <0){
+            res.setTextColor(Color.parseColor("#000000"));
+        }
+        else{
+            res.setTextColor(Color.parseColor("#ffffff"));
+        }
+        res.setText("Sonuç: " + sonuc);
 
     }
 }
